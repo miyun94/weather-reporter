0423e24ef7031d001ea8cfe48defb7efd70b0414
echo <miyunniecho@gmail.com>
2021-01-27 17:22:48 -0800

    localstorage

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,18 +1,23 @@
-fetch('').then(function(response){
-    return response.json();
-}).then(function(data){
-    console.log(data); 
-})
-
 //store search into localstorage 
 $(document).ready(function () {
     $("button").on("click", function () {
-    let citySearch = $('#search').val()
-    console.log(citySearch); 
-    localStorage.setItem("search", (citySearch))
-  })
-  $("#search").append(localStorage.getItem("search"))
+        let citySearch = $('#search').val()
+        console.log(citySearch);
+        localStorage.setItem("search", (citySearch))
+    })
+    $("#search").append(localStorage.getItem("search"))
+})
 
-//
+let citySearch = function(){
+    let searchId = document.getElementById('#search').Value
 
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + searchId + '&appid={API key}')
+    .then(function(response){
+        return response.json(); 
+    })
+    .then(function(response){
+        console.log(reponse.data[0]); 
+
+    })
+}
 
