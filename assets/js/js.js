3da64b98c6bf711b9ea9ebc71bad86539172f81a
echo <miyunniecho@gmail.com>
2021-01-30 23:31:49 -0800

    consolelog

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,23 +1,31 @@
 //store search into localstorage 
 $(document).ready(function () {
     $("button").on("click", function () {
-        let citySearch = $('#search').val()
-        console.log(citySearch);
-        localStorage.setItem("search", (citySearch))
+        let zipCode = $('#searchZip').val()
+        console.log(zipCode);
+        localStorage.setItem("search", (zipCode))
+        citySearch(zipCode);
     })
-    $("#search").append(localStorage.getItem("search"))
+    //$("#searchZip").append(localStorage.getItem("search"))
 })
 
-let citySearch = function(){
-    let searchId = document.getElementById('#search').Value
+//let citySearch = function(searchId){
+    function citySearch(searchId){
+//    let searchId = document.getElementById('#searchZip').val()
+    let searchUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=' + searchId + ',us&appid=fe4afaa3b86128f2033985b903a190ff&units=imperial'
+    $.ajax({
+        url: searchUrl,
+          method: "GET"
+      }).then(function(response){
+        console.log(response);
+        console.log(response.weather[0].description); 
+        console.log(response.main.feels_like); 
+        console.log(response.main.humidity);
+        console.log(response.main.temp); 
+        console.log(response.main.temp_max);
+        console.log(response.main.temp_min);   
+   // fetch('http://api.openweathermap.org/data/2.5/weather?zip=' + searchId + '&appid=fe4afaa3b86128f2033985b903a190ff')
 
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + searchId + '&appid={API key}')
-    .then(function(response){
-        return response.json(); 
-    })
-    .then(function(response){
-        console.log(reponse.data[0]); 
-
+       // return response.json(); 
     })
 }
-
