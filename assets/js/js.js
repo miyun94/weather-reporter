764a7bd760e1ceb984635459f80e8812d1f8bd1c
echo <miyunniecho@gmail.com>
2021-01-31 14:46:45 -0800

    call weather api

--- assets/js/js.js
+++ assets/js/js.js
@@ -5,27 +5,40 @@ $(document).ready(function () {
         console.log(zipCode);
         localStorage.setItem("search", (zipCode))
         citySearch(zipCode);
+        fiveDay(zipCode);
     })
-    //$("#searchZip").append(localStorage.getItem("search"))
 })
 
-//let citySearch = function(searchId){
-    function citySearch(searchId){
-//    let searchId = document.getElementById('#searchZip').val()
+//fetches information for the current day 
+function citySearch(searchId) {
     let searchUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=' + searchId + ',us&appid=fe4afaa3b86128f2033985b903a190ff&units=imperial'
     $.ajax({
         url: searchUrl,
-          method: "GET"
-      }).then(function(response){
+        method: "GET"
+    }).then(function (response) {
         console.log(response);
-        console.log(response.weather[0].description); 
-        console.log(response.main.feels_like); 
-        console.log(response.main.humidity);
-        console.log(response.main.temp); 
-        console.log(response.main.temp_max);
-        console.log(response.main.temp_min);   
-   // fetch('http://api.openweathermap.org/data/2.5/weather?zip=' + searchId + '&appid=fe4afaa3b86128f2033985b903a190ff')
-
-       // return response.json(); 
+        console.log(response.name);
+        console.log(response.main.temp)
+        $(".temp").html(response.main.temp);
+        $(".city-name").html(response.name);
+        $(".weather").html(response.weather.main)
     })
 }
+
+//create another response for 5-day forecast 
+function fiveDay(searchId) {
+        let fiveSearch = 'http://api.openweathermap.org/data/2.5/forecast?zip=' + searchId + ',us&appid=fe4afaa3b86128f2033985b903a190ff&units=imperial'
+        $.ajax({
+            url: fiveSearch,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            //create a loop for the 5 - day forecast to go through
+            for (let i = 0; i < response.list.length; i += 8) {
+                console.log(response.list[i]); 
+            }
+        })
+    }
+
+
+
